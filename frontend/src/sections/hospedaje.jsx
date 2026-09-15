bfd1930aeba34edec4d111d2ee46ef6b51840770
vvalenv <valenvillar.neco@gmail.com>
2026-09-15 17:13:59 -0300

    cambio basico de estructura hospedaje

--- frontend/src/sections/hospedaje.jsx
+++ frontend/src/sections/hospedaje.jsx
@@ -1,7 +1,12 @@
 export const Hospedaje = () => {
     return (
-        <main>
-            <h1>Holas</h1>
+        <main className="main-hospedaje">
+            <section className="hero-section1">
+                <div className="hero-content">
+                    <h1 className="hero-title">Elegí tu forma de vivir pura vida</h1>
+                    <p>Aca cada dia se hace a otro ritmo: mar cerca, comida hecha en casa y gente que llega parra sentirse parte.</p>
+                </div>
+            </section>
         </main>
     )
 }
